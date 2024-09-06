606ad472cc601343cdf942689d4525addf59a79a
khsujon <kamrulhasan.km3@gmail.com>
2024-09-06 11:35:08 +0600

    News App development

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashSceen extends StatelessWidget {
+  const SplashSceen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
